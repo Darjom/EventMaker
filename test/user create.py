4c845152a0527fa542e0e92d1698b4b3abe2b4d6
Darjom <32781694+Darjom@users.noreply.github.com>
2025-04-08 11:54:16 -0400

    update test

--- test/user create.py
+++ test/user create.py
@@ -19,14 +19,15 @@ def test_create_user():
 
     # Ejecutar creación de usuario
     return creator.execute(
-        role_ids=[1],  # Asegúrate que exista este rol en tu DB
+        role_ids=[2],  # Asegúrate que exista este rol en tu DB
         first_name="dan",
         last_name="Reque",
-        email="adm@adm.com",
+        email="mod@modmod.com",
         password="12345678"
     )
 
-
+#adm@adm.com - admin
+#mod@modmod.com - moderador
 if __name__ == "__main__":
     with app.app_context():
         # Crear el usuario
