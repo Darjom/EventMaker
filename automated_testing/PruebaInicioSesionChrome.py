9db0cfb78a79dceeb30608bc6826811c3eba9321
Raid02 <raidcito@gmail.com>
2025-04-22 15:26:40 -0400

    Asignacion  de  ID's al  header

--- automated_testing/PruebaInicioSesionChrome.py
+++ automated_testing/PruebaInicioSesionChrome.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import time
+
 
 def initialize_driver():
     chrome_path = "E:/Usuarios/INTELPC/Documents/GitHub/EventMaker/automated_testing/chromedriver.exe"
