417a7c760c2ca82358b7c64d2de8374cb9a5f2da
apazaJose <130260924+apazaJose@users.noreply.github.com>
2025-05-03 14:10:12 -0400

    add

--- modules/delegations/application/GetStudentIdsByDelegation.py
+++ modules/delegations/application/GetStudentIdsByDelegation.py
@@ -1,11 +1,14 @@
 from modules.delegations.domain.DelegationRepository import DelegationRepository
+from modules.students.application.GetStudentsByListIds import GetStudentsByListIds
+from modules.students.domain.StudentRepository import StudentRepository
 
 
 class GetStudentIdsByDelegation:
-
-    def __init__(self, repository: DelegationRepository):
-        self.__repository = repository
+    def __init__(self, delegation_repository: DelegationRepository, student_repository: StudentRepository):
+        self.__delegation_repository = delegation_repository
+        self.__student_repository = student_repository
 
     def execute(self, delegation_id: int):
-        list_ids = self.__repository.get_student_ids_by_delegation_id(delegation_id)
-        return list_ids
+        list_ids = self.__delegation_repository.get_student_ids_by_delegation_id(delegation_id)
+        students = GetStudentsByListIds(self.__student_repository).execute(list_ids)
+        return students
