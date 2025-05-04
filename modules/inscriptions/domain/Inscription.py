1a4d5d1032b95834451896e6894fe09501dc1830
apazaJose <130260924+apazaJose@users.noreply.github.com>
2025-05-03 22:51:20 -0400

    ..

--- modules/inscriptions/domain/Inscription.py
+++ modules/inscriptions/domain/Inscription.py
@@ -1,4 +1,6 @@
 from datetime import date, time
+from typing import Optional
+
 
 class Inscription:
     """
@@ -8,7 +10,7 @@ class Inscription:
     :param event_id: Identificador del evento.
     :param area_id: Identificador del área.
     :param category_id: Identificador de la categoría.
-    :param delegation_id Infrtificador de la delegation
+    :param delegation_id Identificador de la delegation
     :param inscription_date: Fecha de la inscripción.
     :param status: Estado de la inscripción.
     """
@@ -17,6 +19,7 @@ class Inscription:
                  event_id: int,
                  area_id: int,
                  category_id: int,
+                 delegation_id: Optional[int] = None,
                  inscription_date: date,
                  status: str):
 
@@ -24,6 +27,7 @@ class Inscription:
         self.event_id = event_id
         self.area_id = area_id
         self.category_id = category_id
+        self.delegation_id = delegation_id
         self.inscription_date = inscription_date
         self.status = status
 
