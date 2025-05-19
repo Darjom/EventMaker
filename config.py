4e17ae7d0052d05563e85199c6219d3184155083
Darjom <32781694+Darjom@users.noreply.github.com>
2025-05-19 01:02:50 -0400

    update prod

--- config.py
+++ config.py
@@ -1,8 +1,16 @@
 from decouple import config
-print("DB_PORT:", config('DB_PORT'))
-class Config:
-    SECRET_KEY = config('SECRET_KEY')
-    JWT_SECRET_KEY = config('JWT_SECRET_KEY')
 
-    SQLALCHEMY_DATABASE_URI = f"postgresql://{config('DB_USER')}:{config('DB_PASSWORD')}@{config('DB_HOST')}:{config('DB_PORT')}/{config('DB_NAME')}"
+class Config:
+    DB_NAME = config("DB_NAME", default="postgres")
+    DB_USER = config("DB_USER", default="super")
+    DB_PASSWORD = config("DB_PASSWORD", default="")
+    DB_HOST = config("DB_HOST", default="localhost")
+    DB_PORT = config("DB_PORT", default=5432, cast=int)
+
+    SQLALCHEMY_DATABASE_URI = (
+        f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    )
+
+    SECRET_KEY = config("SECRET_KEY", default="dev-secret")
+    JWT_SECRET_KEY = config("JWT_SECRET_KEY", default="dev-jwt-secret")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
