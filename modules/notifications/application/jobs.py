d484a5031d7991dbe8846230768c8d4b3d0f3f1b
Darjom <32781694+Darjom@users.noreply.github.com>
2025-05-28 22:51:32 -0400

    update notifer

--- modules/notifications/application/jobs.py
+++ modules/notifications/application/jobs.py
@@ -1,7 +1,8 @@
 from modules.notifications.application.EnviarRecordatorioInicio import EnviarRecordatorioInicio
 from modules.notifications.application.EnviarRecordatorioCierre import EnviarRecordatorioCierre
 
-class jobs():
+class Jobs:
+    @staticmethod
     def ejecutar_todas_las_notificaciones(app):
         EnviarRecordatorioInicio.enviar_notificaciones_inicio(app)
         EnviarRecordatorioCierre.enviar_notificaciones_inscripcion(app)
